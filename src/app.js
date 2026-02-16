f41e105be9879477c10b20cc77456487c98a962c
HajarZikiss <hajarzikiss2006@example.com>
2026-02-16 10:12:13 +0000

    conflit

--- src/app.js
+++ src/app.js
@@ -11,10 +11,10 @@ app.use(express.json());
 
 // Database Connection
 mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/marilla_db')
-  .then(() => console.log('✅ Connected to MongoDB'))
-  .catch((err) => console.error('❌ DB Error:', err));
+  .then(() => console.log(' Connected to MongoDB'))
+  .catch((err) => console.error(' DB Error:', err));
 
 app.get('/', (req, res) => res.send('Marilla API is running...'));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server on port ${PORT}`));
+app.listen(PORT, () => console.log(` Server on port ${PORT}`));
