56b1c62349f39c1bb65e6175dae79fe131aafa86
zaynebob <125515675+Zayneb-ob1@users.noreply.github.com>
2026-05-10 18:18:14 +0100

    feat: Add simple admin dashboard for rooms

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 const connectDB = require('./config/db');
 const roomRoutes = require('./routes/roomRoutes');
 
@@ -12,6 +13,7 @@ const app = express();
 // Middlewares
 app.use(cors());
 app.use(express.json()); // Mohim bach yfham JSON
+app.use(express.static(path.join(__dirname, '../public')));
 
 // Database Connection
 connectDB();
